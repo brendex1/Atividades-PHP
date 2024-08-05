5900ed7382d7ddaa697ba14c2508ff28d21db479
Brenda Barbosa <163914343+brendex1@users.noreply.github.com>
2024-08-05 18:18:42 -0300

    Add files via upload

---
+++ aula07.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="recebe-aula07.php?nome=Brenda">Enviar o nome Brenda</a>
+
+    <form action="recebe-aula07.php">
+        <labelfpr="nome">Nome: </label> <br>
+        <input type="text" name="nome"><br>
+        <label for="sobrenome">Sobrenome: </label><br>
+        <input type="text" ame="sobrenome"><br>
+        <input type="submit" value="Enviar">
+    </form>    
+</body>
+</html>
